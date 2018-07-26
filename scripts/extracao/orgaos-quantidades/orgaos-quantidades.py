ead0282f2ff623f42ffe60a193e55e0f1b6a180a
Augusto Herrmann <augusto.herrmann@gmail.com>
2018-07-26 18:58:42 -0300

    script para extrair quantidade de datasets por órgão

---
+++ scripts/extracao/orgaos-quantidades/orgaos-quantidades.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from ckanapi import RemoteCKAN
+
+
+dadosgovbr = RemoteCKAN('http://dados.gov.br')
+
+orgaos_quantidades = [(orgao['display_name'], orgao['package_count']) for orgao in dadosgovbr.action.organization_list(all_fields=True)]
+
+with open('orgaos.csv', 'w') as f:
+    planilha = csv.writer(f)
+    planilha.writerow(('órgão', 'quantidade'))
+    for linha in orgaos_quantidades:
+        planilha.writerow(linha)
+
